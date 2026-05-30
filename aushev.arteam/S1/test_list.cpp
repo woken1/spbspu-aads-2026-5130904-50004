e5dedb228998491fce784559e026f6e93d78fdb7
Ausev.Arteam <seregabest77@gmail.com>
2026-05-30 16:37:33 +0300

    fix build

--- aushev.arteam/S1/test_list.cpp
+++ aushev.arteam/S1/test_list.cpp
@@ -1,5 +1,5 @@
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
+#define BOOST_TEST_MODULE ListTests
+#include <boost/test/included/unit_test.hpp>
 #include "list.h"
 
 BOOST_AUTO_TEST_SUITE(ListTests)
