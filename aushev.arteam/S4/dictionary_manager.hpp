d7ba7b7793f1eb1b446913da3be3e4c9bcfe5f3e
Ausev.Arteam <seregabest77@gmail.com>
2026-06-09 19:43:19 +0300

    Создание сущности для хранения имени словаря и дерева, а также менеджера для загрузки данных из файла с ручным парсингом

---
+++ aushev.arteam/S4/dictionary_manager.hpp
@@ -0,0 +1,140 @@
+#ifndef AUSHEV_DICTIONARY_MANAGER_HPP
+#define AUSHEV_DICTIONARY_MANAGER_HPP
+
+#include <string>
+#include <fstream>
+#include "dictionary_entry.hpp"
+
+namespace aushev {
+
+class DictList {
+    struct Node {
+        DictionaryEntry data_;
+        Node* next_;
+        Node(const std::string& name) : data_(name), next_(nullptr) {}
+    };
+    Node* head_;
+
+public:
+    DictList() : head_(nullptr) {}
+
+    ~DictList() {
+        clear();
+    }
+
+    void clear() {
+        while (head_) {
+            Node* tmp = head_;
+            head_ = head_->next_;
+            delete tmp;
+        }
+    }
+
+    DictionaryEntry* find(const std::string& name) {
+        Node* curr = head_;
+        while (curr) {
+            if (curr->data_.getName() == name) {
+                return &curr->data_;
+            }
+            curr = curr->next_;
+        }
+        return nullptr;
+    }
+
+    bool add(const std::string& name) {
+        if (find(name)) {
+            return false;
+        }
+        Node* newNode = new Node(name);
+        newNode->next_ = head_;
+        head_ = newNode;
+        return true;
+    }
+};
+
+int stringToInt(const std::string& str) {
+    int result = 0;
+    int sign = 1;
+    size_t i = 0;
+    if (i < str.size() && str[i] == '-') {
+        sign = -1;
+        ++i;
+    }
+    for (; i < str.size(); ++i) {
+        result = result * 10 + (str[i] - '0');
+    }
+    return result * sign;
+}
+
+size_t skipSpaces(const std::string& str, size_t pos) {
+    while (pos < str.size() && str[pos] == ' ') {
+        ++pos;
+    }
+    return pos;
+}
+
+std::string readToken(const std::string& str, size_t& pos) {
+    pos = skipSpaces(str, pos);
+    std::string token;
+    while (pos < str.size() && str[pos] != ' ') {
+        token += str[pos];
+        ++pos;
+    }
+    return token;
+}
+
+class DictionaryManager {
+    DictList dicts_;
+
+public:
+    bool loadFromFile(const std::string& filename) {
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            return false;
+        }
+
+        std::string line;
+        while (std::getline(file, line)) {
+            size_t pos = 0;
+            pos = skipSpaces(line, pos);
+            if (pos == line.size()) {
+                continue;
+            }
+
+            std::string name = readToken(line, pos);
+            if (name.empty()) {
+                continue;
+            }
+
+            dicts_.add(name);
+            DictionaryEntry* entry = dicts_.find(name);
+
+            while (pos < line.size()) {
+                std::string keyStr = readToken(line, pos);
+                if (keyStr.empty()) {
+                    break;
+                }
+                std::string valStr = readToken(line, pos);
+                if (valStr.empty()) {
+                    break;
+                }
+
+                int key = stringToInt(keyStr);
+                entry->getTree().push(key, valStr);
+            }
+        }
+        return true;
+    }
+
+    DictionaryEntry* findDict(const std::string& name) {
+        return dicts_.find(name);
+    }
+
+    bool createDict(const std::string& name) {
+        return dicts_.add(name);
+    }
+};
+
+}
+
+#endif
