b96e7e5331da149bb448ae64a1ad20082bb2684a
Ausev.Arteam <seregabest77@gmail.com>
2026-06-26 23:04:22 +0300

    методы управления стадией банов в лобби в draft_analyzer.cpp

---
+++ aushev.arteam/F0/draft_analyzer.cpp
@@ -0,0 +1,37 @@
+#include "draft_analyzer.h"
+#include <cstring>
+#include <cstdio>
+
+namespace aushev {
+
+DraftAnalyzer::DraftAnalyzer(const HashTable& db) :
+  db_(db)
+{
+}
+
+void DraftAnalyzer::setBans(lobby_t& lobby, const char* bans[], std::size_t count)
+{
+  lobby.banCount = (count < MAX_BANS) ? count : MAX_BANS;
+  for (std::size_t i = 0; i < lobby.banCount; ++i) {
+    std::strncpy(lobby.banned[i], bans[i], MAX_NAME - 1);
+    lobby.banned[i][MAX_NAME - 1] = '\0';
+  }
+}
+
+void DraftAnalyzer::clearDraft(lobby_t& lobby)
+{
+  lobby.banCount = 0;
+  std::memset(lobby.banned, 0, sizeof(lobby.banned));
+}
+
+bool DraftAnalyzer::isBanned(const lobby_t& lobby, const char* name) const
+{
+  for (std::size_t i = 0; i < lobby.banCount; ++i) {
+    if (std::strcmp(lobby.banned[i], name) == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+}
